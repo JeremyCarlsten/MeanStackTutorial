d18d77021f37130b1aa6593afa675f861715ce95
JeremyCarlsten <jeremy@jeremycarlsten.com>
2015-06-04 08:51:56 -0500

    Setting things up for Heroku deployment

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 var app = express();
 
 function compile(str, path) {
-  return stylus(str).set('filename', path);
+    return stylus(str).set('filename', path);
 }
 
-var PORT = 3030;
+var PORT = process.env.PORT || 3030;
 
 
 app.set('views', __dirname + '/server/views');
@@ -20,24 +20,29 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
+    src: __dirname + '/public',
+    compile: compile
 }));
 
 app.use(express.static(__dirname + '/public'));
-
-mongoose.connect('mongodb://localhost/meanTutorial');
+console.log("Environment: " + env)
+if (env == 'development') {
+    mongoose.connect('mongodb://localhost/meanTutorial');
+}
+else {
+    mongoose.connect('mongodb://jcarlsten:password@ds043012.mongolab.com:43012/meantutorial');
+}
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection'));
-db.once('open', function(){
-  console.log("database is opened");
+db.once('open', function () {
+    console.log("database is opened");
 });
 
 var messageSchema = mongoose.Schema({message: String});
 var mongoMessage;
 var Message = mongoose.model('Message', messageSchema);
-Message.findOne().exec(function(err, messageDoc){
-  mongoMessage = messageDoc.message;
+Message.findOne().exec(function (err, messageDoc) {
+    mongoMessage = messageDoc.message;
 });
 
 // Notes:
@@ -45,13 +50,13 @@ Message.findOne().exec(function(err, messageDoc){
 //    - route '/*' or '*' accepts all routes
 
 app.get('/partials/:partialsPath', function (request, response) {
-  response.render('partials/' + request.params.partialsPath)
+    response.render('partials/' + request.params.partialsPath)
 });
 
 app.get('*', function (request, response) {
-  response.render('index', {
-    mongoMessage: mongoMessage
-  });
+    response.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
 app.listen(PORT);
