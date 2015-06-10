871e80e9a27315114ed68954250d757134197a20
JeremyCarlsten <jeremy@jeremycarlsten.com>
2015-06-10 08:11:38 -0500

    Adding User Schema to the database

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,23 @@ module.exports = function (config) {
   db.once('open', function () {
     console.log("database is opened");
   });
+
+
+    var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    userName: String
+  });
+
+  var User = mongoose.model('User', userSchema);
+
+  User.find({}).exec(function(err, collection){
+    if(collection.length == 0) {
+      User.create({firstName: "Jeremy", lastName: "Carlsten", userName: "JeremyCarlsten"});
+      User.create({firstName: "James", lastName: "Bond", userName: "007Bond"});
+      User.create({firstName: "Marc ", lastName: "Spagnuolo", userName: "TheWoodWhisperer"});
+      User.create({firstName: "Matt", lastName: "Vanderlist", userName: "MBW"});
+    }
+  });
+
 };
