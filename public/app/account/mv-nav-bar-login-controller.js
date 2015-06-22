9abdbc81637a4b970dd0def2b9f21c4f9ba804c7
JeremyCarlsten <jcc.993@gmail.com>
2015-06-22 08:53:02 -0500

    doing some basic refactoring

--- public/app/account/mv-nav-bar-login-controller.js
+++ public/app/account/mv-nav-bar-login-controller.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('mvNavBarLoginController', function($scope, mvIdentity, mvNotifier, mvAuth){
   $scope.identity = mvIdentity;
   $scope.signin = function(username, password){
-      mvAuth.authenticateuser(username, password).then(function(success){
+      mvAuth.authenticateUser(username, password).then(function(success){
         if(success){
           mvNotifier.notify("You have been successfully logged in!");
         }else{
