4e6a69499dd4306e3733e8a6126e7018def626ee
JeremyCarlsten <jcc.993@gmail.com>
2015-07-13 08:34:33 -0500

    refactoring server.js to be better broken down

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-var express = require('express'),
-    mongoose = require('mongoose'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+var express = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 var app = express();
@@ -10,42 +7,13 @@ var config = require('./server/config/config')[env];
 
 require('./server/config/express')(app, config);
 require('./server/config/mongoose')(config);
-
-
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(function (username, password, done) {
-    User.findOne({userName: username}).exec((function (err, user) {
-        if (user && user.authenticate(password)) {
-            return done(null, user)
-        }
-        else {
-            return done(null, false)
-        }
-    }))
-}));
-
+require('./server/config/passport')();
+require('./server/config/routes')(app);
 
 app.use(function (request, response, next) {
-    console.log(request.user);
+    console.log(request.user); //todo: figure out why I am undefined...
     next();
 });
-
-passport.serializeUser(function (user, done) {
-    if (user) done(null, user._id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findOne({__id: id}).exec(function (err, user) {
-        if (user) {
-            return done(null, user);
-        }
-        else {
-            return done(null, false);
-        }
-    });
-});
-
-require('./server/config/routes')(app);
 
 app.listen(config.port);
 console.log("Listening on port " + config.port + " ...");
